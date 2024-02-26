f9641af2b57852aec534fab792410cb4520dae31
mattyhempstead <matty.hempstead@gmail.com>
2024-02-27 01:30:35 +1100

    Add growth rate plot

---
+++ plot.py
@@ -0,0 +1,37 @@
+import random
+import matplotlib.pyplot as plt
+from pyfck import encode_program
+
+random.seed(0)
+
+def get_encoded_program_length(n):
+    """
+        Returns the length of an encoded program of length n.
+    """
+
+    # Generate random string
+    # Technically all of unicode is supported, but a restricted charset is a good enough approximation
+    available_chars = [chr(i) for i in range(0, 255)]
+    random_program_string = ''.join(random.choice(available_chars) for _ in range(n))
+
+    encoded_program = encode_program(random_program_string)
+    return len(encoded_program)
+
+
+# Generate the series of integers for x from 1 to 100
+x_values = range(1, 101)
+y_values = [get_encoded_program_length(x) for x in x_values]
+
+print("Approximate gradient:", y_values[-1] / x_values[-1])
+
+
+# Plotting
+plt.plot(x_values, y_values, marker='o')  # Use marker='o' for circular markers on each point
+
+# Adding title and labels
+plt.title('Growth rate of restricted charset program lengths')
+plt.xlabel('Length of original program')
+plt.ylabel('Length of restricted charset program')
+
+# Show the plot
+plt.show()
